25e73d11d52c6901486120ad47d9748ea3791a72
Ishra Khanam <ishra6107@gmail.com>
2025-09-08 10:12:46 +0530

    All changes complete after testing
    All changes complete after testing

--- src/pages/home/howItsWork.jsx
+++ src/pages/home/howItsWork.jsx
@@ -22,53 +22,53 @@ const HomeHowitworks = ({ isActive, isExiting }) => {
   const Auth = JSON?.parse(localStorage.getItem('Auth') ?? '{}');
 
 
-const [isMobile, setIsMobile] = useState(() => window.innerWidth <= 768);
-const [isDesktop, setIsDesktop] = useState(() => window.innerWidth > 768);
+  const [isMobile, setIsMobile] = useState(() => window.innerWidth <= 768);
+  const [isDesktop, setIsDesktop] = useState(() => window.innerWidth > 768);
 
   // const { ContextFaqsDataAPI } = useContext(UserContext);
 
   // =================================
   //       API FUNCTIONALITY
   // =================================
-// const [collapsed, setCollapsed] = useState(false);
+  // const [collapsed, setCollapsed] = useState(false);
 
-// useEffect(() => {
-//   if (step >= 6) {
-//     setTimeout(() => setCollapsed(true), 1200)
-//   }
-// }, [step]);
+  // useEffect(() => {
+  //   if (step >= 6) {
+  //     setTimeout(() => setCollapsed(true), 1200)
+  //   }
+  // }, [step]);
 
-// const handleButtonClick = () => {
-//   setCollapsed((prev) => !prev);
-// };
+  // const handleButtonClick = () => {
+  //   setCollapsed((prev) => !prev);
+  // };
 
 
-//============= Expand/Collapse 
+  //============= Expand/Collapse 
 
-const [collapsed, setCollapsed] = useState(false);
-// const [animating, setAnimating] = useState(false);
+  const [collapsed, setCollapsed] = useState(false);
+  // const [animating, setAnimating] = useState(false);
 
-// useEffect(() => {
-//   if (step >= 6) {
-//     // Jab timeline complete ho jaaye, rocket ko move karao
-//     setAnimating(true);
-//     setTimeout(() => {
-//       setCollapsed(true);   // section hide
-//       setAnimating(false);
-//     }, 1200); // rocket animation duration ke equal rakho
-//   }
-// }, [step]);
+  // useEffect(() => {
+  //   if (step >= 6) {
+  //     // Jab timeline complete ho jaaye, rocket ko move karao
+  //     setAnimating(true);
+  //     setTimeout(() => {
+  //       setCollapsed(true);   // section hide
+  //       setAnimating(false);
+  //     }, 1200); // rocket animation duration ke equal rakho
+  //   }
+  // }, [step]);
 
-// const handleButtonClick = () => {
-//   setAnimating(true);
-//   setCollapsed((prev) => !prev);
+  // const handleButtonClick = () => {
+  //   setAnimating(true);
+  //   setCollapsed((prev) => !prev);
 
-//   // jab expand kare, rocket vapas aaye
-//   setTimeout(() => {
-//     setAnimating(false);
-//   }, 1200);
-// };
-//============= Expand/Collapse end
+  //   // jab expand kare, rocket vapas aaye
+  //   setTimeout(() => {
+  //     setAnimating(false);
+  //   }, 1200);
+  // };
+  //============= Expand/Collapse end
 
   const HandleAPI = async () => {
     try {
@@ -160,17 +160,17 @@ const [collapsed, setCollapsed] = useState(false);
     }
   }, [isExiting]);
 
-useEffect(() => {
-  const handleResize = () => {
-    setIsMobile(window.innerWidth <= 768);
-    setIsDesktop(window.innerWidth > 768);
-  };
-  window.addEventListener('resize', handleResize);
-  return () => window.removeEventListener('resize', handleResize);
-}, []);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+      setIsDesktop(window.innerWidth > 768);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
 
-  
+
   return (
     <section
       id="HowItWorks_Section"
@@ -198,149 +198,149 @@ useEffect(() => {
         )} */}
 
         {/*  Desktop View (Horizontal Timeline) */}
-      <div className={`timeline-wrapper ${collapsed ? 'collapsed' : 'expanded'}`}>
+        <div className={`timeline-wrapper ${collapsed ? 'collapsed' : 'expanded'}`}>
           {showSteps && !isMobile && (
-          <div
-            className="howitwork-second h-100 d-flex align-items-center justify-content-center"
-            data-aos="zoom-in-up"
-          >
-            <div className="row text-center position-relative inner-row-index">
-              {/* Step 1 */}
-              <div className={`col-12 col-md-4 howitworks-step ${step >= 1 ? 'visible' : ''}`}>
-                <h6 className="montserrat-bold font-20 mb-22 text-blue  lh-sm">
-                  Launch Your Cosmic Journey
-                </h6>
-                <p className="montserrat-regular font-18 text-blue  lh-sm">
-                  Start your adventure by signing up and setting course for your first galaxy.
-                </p>
-              </div>
+            <div
+              className="howitwork-second h-100 d-flex align-items-center justify-content-center"
+              data-aos="zoom-in-up"
+            >
+              <div className="row text-center position-relative inner-row-index">
+                {/* Step 1 */}
+                <div className={`col-12 col-md-4 howitworks-step ${step >= 1 ? 'visible' : ''}`}>
+                  <h6 className="montserrat-bold font-20 mb-22 text-blue  lh-sm">
+                    Launch Your Cosmic Journey
+                  </h6>
+                  <p className="montserrat-regular font-18 text-blue  lh-sm">
+                    Start your adventure by signing up and setting course for your first galaxy.
+                  </p>
+                </div>
 
-              {/* Planet 2 */}
-              <div
-                className={`col-12 col-md-4 d-flex align-items-end justify-content-center howitworks-step ${step >= 3 ? 'visible' : ''
-                  }`}
-              >
-                <img src={Planet2} className="planet-width mb-3" alt="Planet 2" />
-              </div>
+                {/* Planet 2 */}
+                <div
+                  className={`col-12 col-md-4 d-flex align-items-end justify-content-center howitworks-step ${step >= 3 ? 'visible' : ''
+                    }`}
+                >
+                  <img src={Planet2} className="planet-width mb-3" alt="Planet 2" />
+                </div>
 
-              {/* Step 3 */}
-              <div className={`col-12 col-md-4 howitworks-step ${step >= 5 ? 'visible' : ''}`}>
-                <h6 className="montserrat-bold font-20 mb-22 text-blue lh-sm">
-                  Discover New Worlds & Unlock Rewards
-                </h6>
-                <p className="montserrat-regular font-18 text-blue lh-sm">
-                  Every galaxy you explore holds new treasures!
-                </p>
-              </div>
+                {/* Step 3 */}
+                <div className={`col-12 col-md-4 howitworks-step ${step >= 5 ? 'visible' : ''}`}>
+                  <h6 className="montserrat-bold font-20 mb-22 text-blue lh-sm">
+                    Discover New Worlds & Unlock Rewards
+                  </h6>
+                  <p className="montserrat-regular font-18 text-blue lh-sm">
+                    Every galaxy you explore holds new treasures!
+                  </p>
+                </div>
 
-              {/* Timeline */}
-              <div className="col-12 my-4 position-relative">
-                <div className={`timeline-dot ${step >= 1 ? 'visible' : ''}`} />
-                <div className={`timeline-dot ${step >= 3 ? 'visible' : ''}`} />
-                <div className={`timeline-dot ${step >= 4 ? 'visible' : ''}`} />
-                <div className="timeline-border" />   
-  <img src={Rocketgif} alt="Rocket"   className={`rocket-gif ${collapsed ? 'collapse-to-button' : ''}`}  />
-              </div>
+                {/* Timeline */}
+                <div className="col-12 my-4 position-relative">
+                  <div className={`timeline-dot ${step >= 1 ? 'visible' : ''}`} />
+                  <div className={`timeline-dot ${step >= 3 ? 'visible' : ''}`} />
+                  <div className={`timeline-dot ${step >= 4 ? 'visible' : ''}`} />
+                  <div className="timeline-border" />
+                  <img src={Rocketgif} alt="Rocket" className={`rocket-gif ${collapsed ? 'collapse-to-button' : ''}`} />
+                </div>
 
-              {/* Planet 1 */}
-              <div className={`col-12 col-md-4 howitworks-step ${step >= 2 ? 'visible' : ''}`}>
-                <img src={Planet1} className="planet-width mt-3" alt="Planet 1" />
-              </div>
+                {/* Planet 1 */}
+                <div className={`col-12 col-md-4 howitworks-step ${step >= 2 ? 'visible' : ''}`}>
+                  <img src={Planet1} className="planet-width mt-3" alt="Planet 1" />
+                </div>
 
-              {/* Step 2 */}
-              <div className={`col-12 col-md-4 howitworks-step ${step >= 4 ? 'visible' : ''}`}>
-                <h6 className="montserrat-bold font-20 mb-22 text-blue  lh-sm">
-                  Invite a Friend & Travel Together
-                </h6>
-                <p className="montserrat-regular font-18 text-blue  lh-sm">
-                  Space expeditions are more thrilling with a co-pilot!
-                </p>
-              </div>
+                {/* Step 2 */}
+                <div className={`col-12 col-md-4 howitworks-step ${step >= 4 ? 'visible' : ''}`}>
+                  <h6 className="montserrat-bold font-20 mb-22 text-blue  lh-sm">
+                    Invite a Friend & Travel Together
+                  </h6>
+                  <p className="montserrat-regular font-18 text-blue  lh-sm">
+                    Space expeditions are more thrilling with a co-pilot!
+                  </p>
+                </div>
 
-              {/* Planet 3 */}
-              <div className={`col-12 col-md-4 howitworks-step ${step >= 6 ? 'visible' : ''}`}>
-                <img src={Planet3} className="planet-width mt-3" alt="Planet 3" />
+                {/* Planet 3 */}
+                <div className={`col-12 col-md-4 howitworks-step ${step >= 6 ? 'visible' : ''}`}>
+                  <img src={Planet3} className="planet-width mt-3" alt="Planet 3" />
+                </div>
               </div>
             </div>
-          </div>
-        )}
+          )}
 
-        {/* Mobile View (Vertical Timeline) */}
-        {showSteps && isMobile && (
-          // Mobile View
-          <div className="timeline-mobile">
-            <div className="timeline-vertical">
-              <div className="timeline-line-vertical"></div>
-
-              <div className="rocket-vertical">                
-  <img src={Rocketgif} alt="Rocket"   className={` ${collapsed ? 'collapse-to-button' : ''}`}  />
-              </div>
-
-              {/* Timeline dots */}
-              <div className={`timeline-dot-mobile ${step >= 1 ? 'visible' : ''}`} style={{ top: '15%', transition: "opacity 0.5s ease-in" }}></div>
-              <div className={`timeline-dot-mobile ${step >= 3 ? 'visible' : ''}`} style={{ top: '45%', transition: "opacity 0.5s ease-in" }}></div>
-              <div className={`timeline-dot-mobile ${step >= 5 ? 'visible' : ''}`} style={{ top: '75%', transition: "opacity 0.5s ease-in" }}></div>
-
-              <div className="timeline-mobile">
+          {/* Mobile View (Vertical Timeline) */}
+          {showSteps && isMobile && (
+            // Mobile View
+            <div className="timeline-mobile">
+              <div className="timeline-vertical">
                 <div className="timeline-line-vertical"></div>
 
-                {/* Step 1 → Left Planet | Right Text */}
-                <div className={`step-mobile ${step >= 1 ? 'visible' : ''}`}>
-                  <div className="step-box left">
-                    <img src={Planet1} className="planet-svg" alt="Planet 1" />
-                  </div>
-                  <div className="step-box right">
-                    <div className="step-text">
-                      <h6 className="step-title montserrat-bold font-12 text-blue">Launch Your Cosmic Journey</h6>
-                      <p className="step-description montserrat-regular font-12 text-blue pb-5">
-                        Start your adventure by signing up and setting course for your first galaxy.
-                        Every star you navigate brings you closer to exciting rewards. Ready to explore?
-                      </p>
-                    </div>
-                  </div>
+                <div className="rocket-vertical">
+                  <img src={Rocketgif} alt="Rocket" className={` ${collapsed ? 'collapse-to-button' : ''}`} />
                 </div>
 
-                {/* Step 2 → Left Text | Right Planet */}
-                <div className={`step-mobile ${step >= 2 ? 'visible' : ''}`}>
-                  <div className="step-box left">
-                    <div className="step-text">
-                      <h6 className="step-title text-start  montserrat-bold font-12 text-blue ">Invite a Friend & Travel Together</h6>
-                      <p className="step-description text-start  montserrat-regular font-12 text-blue ">
-                        Space expeditions are more thrilling with a co-pilot! Invite a friend to join the journey,
-                        and you’ll both earn points as you explore the universe together.
-                      </p>
+                {/* Timeline dots */}
+                <div className={`timeline-dot-mobile ${step >= 1 ? 'visible' : ''}`} style={{ top: '10%', transition: "opacity 0.5s ease-in" }}></div>
+                <div className={`timeline-dot-mobile ${step >= 3 ? 'visible' : ''}`} style={{ top: '45%', transition: "opacity 0.5s ease-in" }}></div>
+                <div className={`timeline-dot-mobile ${step >= 5 ? 'visible' : ''}`} style={{ top: '75%', transition: "opacity 0.5s ease-in" }}></div>
+
+                <div className="timeline-mobile">
+                  <div className="timeline-line-vertical"></div>
+
+                  {/* Step 1 → Left Planet | Right Text */}
+                  <div className={`step-mobile ${step >= 1 ? 'visible' : ''}`}>
+                    <div className="step-box left">
+                      <img src={Planet1} className="planet-svg planet-one" alt="Planet 1" />
+                    </div>
+                    <div className="step-box right">
+                      <div className="step-text">
+                        <h6 className="step-title montserrat-bold font-12 text-blue step-title-one">Launch Your Cosmic Journey</h6>
+                        <p className="step-description montserrat-regular font-12 text-blue pb-5">
+                          Start your adventure by signing up and setting course for your first galaxy.
+                          Every star you navigate brings you closer to exciting rewards. Ready to explore?
+                        </p>
+                      </div>
                     </div>
                   </div>
-                  <div className="step-box right">
-                    <img src={Planet2} className="planet-svg" alt="Planet 2" />
-                  </div>
-                </div>
 
-                {/* Step 3 → Left Planet | Right Text */}
-                <div className={`step-mobile ${step >= 3 ? 'visible' : ''}`}>
-                  <div className="step-box left">
-                    <img src={Planet3} className="planet-svg" alt="Planet 3" />
+                  {/* Step 2 → Left Text | Right Planet */}
+                  <div className={`step-mobile ${step >= 2 ? 'visible' : ''}`}>
+                    <div className="step-box left">
+                      <div className="step-text">
+                        <h6 className="step-title text-start  montserrat-bold font-12 text-blue ">Invite a Friend & Travel Together</h6>
+                        <p className="step-description text-start  montserrat-regular font-12 text-blue ">
+                          Space expeditions are more thrilling with a co-pilot! Invite a friend to join the journey,
+                          and you’ll both earn points as you explore the universe together.
+                        </p>
+                      </div>
+                    </div>
+                    <div className="step-box right">
+                      <img src={Planet2} className="planet-svg planet-two" alt="Planet 2" />
+                    </div>
                   </div>
-                  <div className="step-box right p-0">
-                    <div className="step-text">
-                      <h6 className="step-title  montserrat-bold font-12 text-blue">Discover New Worlds & Unlock Rewards</h6>
-                      <p className="step-description  montserrat-regular font-12 text-blue">
-                        Every galaxy you explore holds new treasures! Collect points along the way and redeem
-                        them for exclusive rewards. The final cosmic gem awaits, keep exploring!
-                      </p>
+
+                  {/* Step 3 → Left Planet | Right Text */}
+                  <div className={`step-mobile ${step >= 3 ? 'visible' : ''}`}>
+                    <div className="step-box left">
+                      <img src={Planet3} className="planet-svg" alt="Planet 3" />
+                    </div>
+                    <div className="step-box right p-0">
+                      <div className="step-text">
+                        <h6 className="step-title  montserrat-bold font-12 text-blue">Discover New Worlds & Unlock Rewards</h6>
+                        <p className="step-description  montserrat-regular font-12 text-blue">
+                          Every galaxy you explore holds new treasures! Collect points along the way and redeem
+                          them for exclusive rewards. The final cosmic gem awaits, keep exploring!
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
 
       </div>
 
-{/* Right side expand button */}
-  {/* <div className="get-started-btn-container">
+      {/* Right side expand button */}
+      {/* <div className="get-started-btn-container">
 <Button
   label={
     <span className="flex items-center gap-2">
